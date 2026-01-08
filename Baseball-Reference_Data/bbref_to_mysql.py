f6c9262b6eebd714a8ec3f43b90cffdc9507c91b
AQUAiTE <francisiwnlschool@gmail.com>
2026-01-07 17:51:45 -0800

    Loaded player data into MySQL

--- Baseball-Reference_Data/bbref_to_mysql.py
+++ Baseball-Reference_Data/bbref_to_mysql.py
@@ -8,6 +8,9 @@ load_dotenv()
 
 mlb_standings = pd.read_csv('mlb_standings_2021_2025.csv')
 team_batting = pd.read_csv('team_batting_2021_2025.csv')
+bwar_pitcher_data = pd.read_csv('bwar_pitching_data.csv')
+bwar_batting_data = pd.read_csv('bwar_batting_data.csv')
+
 
 def insert_data(df: pd.DataFrame, table: str, primary_keys: list, conn, cursor):
   columns = ', '.join([f'`{col}`' for col in df.columns])
@@ -35,6 +38,8 @@ try:
     # Insert the standings and team batting data
     insert_data(mlb_standings, 'mlb_team_win_loss', primary_keys=['Tm', 'season'], conn=conn, cursor=cursor)
     insert_data(team_batting, 'bref_team_batting', primary_keys=['season', 'team_abbrev'], conn=conn, cursor=cursor)
+    insert_data(bwar_pitcher_data, 'bref_pitcher_data', primary_keys=['mlb_ID', 'year_ID', 'team_ID'], conn=conn, cursor=cursor)
+    insert_data(bwar_batting_data, 'bref_batter_data', primary_keys=['mlb_ID', 'year_ID', 'team_ID'], conn=conn, cursor=cursor)
 
 
 except connector.Error as e:
